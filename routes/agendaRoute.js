79ee609b911926395515f069b8490297a822ff7d
Mustafa Hassan <mustafawhassan@gmail.com>
2019-11-18 16:45:11 -0500

    simplified agenda routes

--- routes/agendaRoute.js
+++ routes/agendaRoute.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 const agendaController = require('../controllers/agendaController')
 
-router.get('/agenda', agendaController.index)
-router.get('/agenda/id/:_id', agendaController.findById)
-router.post('/agenda/', agendaController.create)
-router.put('/agenda/id/:_id', agendaController.updateById)
+router.get('/', agendaController.index)
+router.get('/id/:_id', agendaController.findById)
+router.post('/', agendaController.create)
+router.put('/id/:_id', agendaController.updateById)
 router.delete('/id/:_id', agendaController.deleteById)
 
 module.exports = router
