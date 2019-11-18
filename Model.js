ede43f773232fb85ccaf4df7456553017b8bd8d1
Tissa Khosla <tissa.music@gmail.com>
2019-11-18 12:09:31 -0500

    adjusted typo in model

--- Model.js
+++ Model.js
@@ -1,12 +1,6 @@
 const mongoose = require("../db/connection.js");
 const Schema = mongoose.Schema;
 
-const skillSchema = new Schema({
-  skill: String,
-  proficiency: Number,
-  examples: [String]
-})
-
 const contactSchema = new Schema({
   firstName: String,
   lastName: String,
