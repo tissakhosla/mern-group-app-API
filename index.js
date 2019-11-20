a6ae7eae9db7ab1ff6e224fb6f601722774044df
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-11-20 12:23:44 -0500

    passport initialized in indexjs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const passport = require('./config/passport')()
 const parser = require("body-parser");
 const routeIndex = require ("./routes/routeIndex");
 
+app.use(passport.initialize())
 app.use(parser.json());
 app.use(cors());
 app.use(routeIndex);
